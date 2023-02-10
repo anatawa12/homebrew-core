b7072ccc73040f54f819fc9fcfb294493013cfba
anatawa12 <anatawa12@icloud.com>
2023-02-10 21:59:32 +0900

    vrc-get 0.1.6 (new formula)

---
+++ Formula/vrc-get.rb
@@ -0,0 +1,24 @@
+class VrcGet < Formula
+  desc "Command-line VPM Client"
+  homepage "https://github.com/anatawa12/vrc-get"
+  url "https://github.com/anatawa12/vrc-get/archive/0.1.6.tar.gz"
+  sha256 "3d1b8735ef837f2237e4bc4978ccf779ae5821c50d5ba6ee557a1761444bad18"
+  license "MIT"
+
+  head "https://github.com/anatawa12/vrc-get.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix, "--path", "."
+  end
+
+  test do
+    assert_match "vrc-get", shell_output("#{bin}/vrc-get --help")
+  end
+end
