69dc24b22bff61de149fc0c121dd388694a6b73c
anatawa12 <anatawa12@icloud.com>
2023-08-20 17:16:18 +0900

    fix vrc-get doesn't include submodules

--- Formula/vrc-get.rb
+++ Formula/vrc-get.rb
@@ -1,8 +1,8 @@
 class VrcGet < Formula
   desc "Command-line VPM Client"
   homepage "https://github.com/anatawa12/vrc-get"
-  url "https://github.com/anatawa12/vrc-get/archive/v1.1.3.tar.gz"
-  sha256 "3bcf9ff026e2c14694db1a55f5be45fba2d8d4b9b51b489dda3b10fb7fc40ae6"
+  url "https://github.com/anatawa12/vrc-get/releases/download/v1.1.3/vrc-get-1.1.3.tar.gz"
+  sha256 "b675ee5148b0b73b0699b2ba2929d1724900becb1dd20acb184c24e959355395"
   license "MIT"
 
   head "https://github.com/anatawa12/vrc-get.git", branch: "master"
