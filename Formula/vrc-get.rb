b8e9b06704933ec419c3ad325a796340221581d1
anatawa12 <anatawa12@icloud.com>
2024-01-05 06:20:40 +0900

    vrc-get 1.4.3-beta.1

--- Formula/vrc-get.rb
+++ Formula/vrc-get.rb
@@ -1,8 +1,8 @@
 class VrcGet < Formula
   desc "Command-line VPM Client"
   homepage "https://github.com/anatawa12/vrc-get"
-  url "https://github.com/anatawa12/vrc-get/archive/v1.4.2.tar.gz"
-  sha256 "f2ee500352423395496ef8a2739e7fac0d97b0bdb1c43e74213bfd494c7e810f"
+  url "https://github.com/anatawa12/vrc-get/archive/refs/tags/v1.4.3-beta.1.tar.gz"
+  sha256 "4a61eec0004e0122abfc609aad4ea56ad4ee235087a9cb66e23ba4b4fc3bfee7"
   license "MIT"
 
   head "https://github.com/anatawa12/vrc-get.git", branch: "master"
