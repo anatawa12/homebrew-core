ec14b727837d7d3317e8c0e5668ae14018422269
anatawa12 <anatawa12@icloud.com>
2024-01-05 05:29:44 +0900

    Update vrc-get.rb

--- Formula/vrc-get.rb
+++ Formula/vrc-get.rb
@@ -15,7 +15,7 @@ class VrcGet < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--root", prefix, "--path", "."
+    system "cargo", "install", "--root", prefix, "--path", "vrc-get"
   end
 
   test do
